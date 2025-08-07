7395feb6bffdb0f4fcb34ddbb41d933fc8b01079
Shawn Han <shawn230106@gmail.com>
2025-08-07 16:09:05 +1000

    Initial commit

---
+++ src/components/my-name.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="my-name">
+    <h2>My name is Shawn Han</h2>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.my-name {
+  margin-top: 20px;
+  font-weight: bold;
+}
+</style>
